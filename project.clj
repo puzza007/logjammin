49e08eaed4c6c314e9b151e40c7d0136d3cc53f2
Aaron France <aaron.l.france@gmail.com>
2015-01-05 15:03:23 +0100

    Update assembly

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject logjammin "0.1.0-SNAPSHOT"
+(defproject logjammin "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/puzza007/logjammin.git"
+  :uberjar-name "logjammin.jar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
