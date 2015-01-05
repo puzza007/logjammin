d95f5b6fa2b7df705dbc5b98a5e5cddb45122e58
Aaron France <aaron.l.france@gmail.com>
2015-01-05 15:07:22 +0100

    Uberjar settings

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :uberjar-name "logjammin.jar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :pom-location "target/"
+  :main ^:skip-aot logjammin.core
+  :target-path "target/%s"
   :dependencies [[com.stuartsierra/component "0.2.2"]
                  [org.clojure/clojure "1.6.0"]
                  [sqs-comp "0.0.1"]])
